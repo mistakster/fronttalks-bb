23b05a04545f5f78a3627f73a89331d0a6a10e86
mista_k <mistakster@gmail.com>
2013-04-07 13:12:01 +0600

    bootstraping data; highlight active file

--- js/collections.js
+++ js/collections.js
@@ -145,15 +145,8 @@ var App = {};
 
 (function () {
 
-  var data = [
-    {"id": 1, "name": "File 1"},
-    {"id": 2, "name": "File 2"},
-    {"id": 2, "name": "File 2"},
-    {"id": 3, "name": "File 3"},
-    {"id": 4, "name": "File 4"},
-    {"id": 5, "name": "File 5"},
-    {"id": 6, "name": "File 6"}
-  ];
+  var data = JSON.parse(localStorage.getItem("files"));
+  data = _.isArray(data) ? data : [];
 
   var files = new App.Files(data);
 
@@ -169,15 +162,30 @@ var App = {};
     });
   });
 
-  $("#refresh").on("click", function () {
-    var brandNewData = data.slice();
+
+
+  $("#refresh-files").on("click", function () {
+    var data = files.toJSON();
+    var maxId = 0;
+    if (data.length) {
+      maxId = _.chain(data).map(function (file) {
+        return file.id;
+      }).max().value();
+    }
     for (var i = 0; i < Math.random() * 3; i++) {
-      brandNewData.push({
-        id: i + 7,
-        name: "File " + (i + 7)
+      var id = i + maxId + 1;
+      data.push({
+        id: id,
+        name: "File " + id
       });
     }
-    files.reset(brandNewData);
+    files.reset(data);
+    localStorage.setItem("files", JSON.stringify(data));
+  });
+
+  $("#clear-files").on("click", function () {
+    localStorage.removeItem("files");
+    files.reset();
   });
 
 }());
