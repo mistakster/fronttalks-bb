65ba0b9333deee683fabcc81f2b558a4200a22ab
Vladimir Kuznetsov <mistakster@gmail.com>
2013-04-16 14:38:24 +0600

    remove unnecessary code and new lines

--- js/poly/itemview.js
+++ js/poly/itemview.js
@@ -26,77 +26,44 @@
 
 
   App.ItemNewsView = App.ItemView.extend({
-
     className: getClassName("news"),
-
-    initialize: function () {
-      App.ItemView.prototype.initialize.apply(this, arguments);
-    },
-
     getTemplate: function () {
       return _.template('<h2><%- head %></h2><div><%- text %></div>');
     }
-
   });
 
+
   App.ItemActionView = App.ItemView.extend({
-
     className: getClassName("action"),
-
-    initialize: function () {
-      App.ItemView.prototype.initialize.apply(this, arguments);
-    },
-
     getTemplate: function () {
       return _.template('<strong><%- user %></strong> makes unknown action on you');
     }
-
   });
 
   App.ItemActionLikeView = App.ItemActionView.extend({
-
-    initialize: function () {
-      App.ItemActionView.prototype.initialize.apply(this, arguments);
-    },
-
     getTemplate: function () {
       return _.template('<i class="icon-heart"></i> <strong><%- user %></strong> likes your post');
     }
-
   });
 
   App.ItemActionUnfollowView = App.ItemActionView.extend({
-
-    initialize: function () {
-      App.ItemActionView.prototype.initialize.apply(this, arguments);
-    },
-
     getTemplate: function () {
       return _.template('<strong><%- user %></strong> is not follow you anymore');
     }
-
   });
 
 
   App.ItemMessageView = App.ItemView.extend({
-
     className: getClassName("message"),
-
     events: {
       "click .item__reply": function () {
         alert("reply to item #" + this.model.id);
       }
     },
-
-    initialize: function () {
-      App.ItemView.prototype.initialize.apply(this, arguments);
-    },
-
     getTemplate: function () {
       return _.template('<span class="pull-right btn item__reply">Reply</span> ' +
         '<strong><%- user %></strong><div><%- text %></div>');
     }
-
   });
 
 
